6e75e217e9465e757fb828abe7e0b39c11d05e84
WHJ <760503653@qq.com>
2017-11-10 16:34:33 +0800

    Initial Commit

---
+++ HJRatingViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HJRatingViewDemo
+//
+//  Created by WHJ on 2017/11/10.
+//  Copyright © 2017年 WHJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
